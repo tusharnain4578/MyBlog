155cf49c32b0a39f7708076981c3927df02df8ac
Tushar Nain <naintushar@hotmail.com>
2022-03-12 20:55:23 +0530

    added dark mode properly

--- model.js
+++ model.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 //MONGODB Connection
 // mongodb+srv://naintushar:tushar123@myblog.edl2v.mongodb.net/myblog
+// mongodb://127.0.0.1:27017/myblog
 mongoose.connect("mongodb+srv://naintushar:tushar123@myblog.edl2v.mongodb.net/myblog");
 
 //Posts Collection Model
