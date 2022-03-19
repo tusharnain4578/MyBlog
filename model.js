cc21c07e8c8f8d9ee4591c64b76c71db122ea3ac
Tushar Nain <naintushar@hotmail.com>
2022-03-19 10:23:15 +0530

    added loader

--- model.js
+++ model.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 //MONGODB Connection
 
 mongoose.connect("mongodb://127.0.0.1:27017/myblog");
-// mongoose.connect("mongodb+srv://naintushar:tushar123@myblog.edl2v.mongodb.net/myblog");
 
 //Posts Collection Model
 const blogSchema = new mongoose.Schema({
